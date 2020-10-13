80e0e63796dafa0022637b0b50fd48153004bdd8
hmn4 <hoang_minh_nguyen_2@sfu.ca>
2020-10-13 11:48:01 -0700

    Update Constants.cpp

--- tools/jsonParser/Constants.cpp
+++ tools/jsonParser/Constants.cpp
@@ -1,24 +1,20 @@
 #include "Constants.h"
 
-Weapon::Weapon(string name, string beats)
-{
+Weapon::Weapon(string name, string beats){
     this->name = name;
     this->beats = beats;
 }
-void Constants:: addWeapon(Weapon weapon)
-{   
+void Constants:: addWeapon(Weapon weapon){   
     weapons.push_back(weapon);
 }
-void Weapon:: print()
-{
+void Weapon:: print(){
     cout << "Name " << name << endl;
     cout << "Beats " << beats << endl;
 }
 
 Constants::Constants(){};
 
-void Constants::print()
-{
+void Constants::print(){
     for (int i = 0; i < weapons.size(); i++)
     {
         weapons[i].print();
