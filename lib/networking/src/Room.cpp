57ecc0d176cd0bbff0c0062f988f1dcc03a347c6
John Wu <wujohnw@sfu.ca>
2020-10-18 12:33:20 -0700

    merged together chatroom prototypes into one branch

--- lib/networking/src/Room.cpp
+++ lib/networking/src/Room.cpp
@@ -3,6 +3,7 @@
 
 void Room::addUser(User user) {
     std::cout << "Adding User: " << user.connection.id << " to room " << roomId << "\n";
+    user.roomId = this->roomId;
     users.push_back(user);
 }
 
@@ -11,6 +12,9 @@ void Room::removeUser(User user) {
     auto eraseBegin = std::remove(std::begin(users), std::end(users), user);
     users.erase(eraseBegin, std::end(users));
 }
+//bool Room::containsUser(User user){
+//
+//}
 void Room::printUsers(){
     std::cout<<"Room:"<<roomId<<"\n";
 
