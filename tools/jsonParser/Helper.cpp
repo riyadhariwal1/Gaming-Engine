739e0537bf6d18df723d4fc754374fc628f6a8ea
Sunaina <spaudel@sfu.ca>
2020-11-12 16:03:26 -0800

    small mistake fix

--- tools/jsonParser/Helper.cpp
+++ tools/jsonParser/Helper.cpp
@@ -5,7 +5,7 @@ string replaceInString(string input, GameVariant value){
   size_t open = input.find("{");
   size_t close = input.find("}");
 
-// shift through every boost type case 
+// shift through every boost type case
   string to_replace = to_string(boost::get<int>(value));
   input.replace(open, close-open+1, to_replace);
 
